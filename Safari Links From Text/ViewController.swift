1dc0f11ae63315a04fda2cb8120433b7c761513b
Koray Birand <info@koraybirand.com>
2019-01-25 16:41:32 +0300

    Initial Commit

---
+++ Safari Links From Text/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Safari Links From Text
+//
+//  Created by Koray Birand on 25.01.2019.
+//  Copyright © 2019 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
