66d6d07893ecfd83a1e2b4a34517657a5555cc82
Koray Birand <info@koraybirand.com>
2019-01-25 17:26:04 +0300

    1st Commit

--- Safari Links From Text/ViewController.swift
+++ Safari Links From Text/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fileURL = URL(fileURLWithPath: "/Users/koraybirand/Desktop/history.txt")
+        
+        do {
+            let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+            let newArray = text2.components(separatedBy: "\n")
+            for items in newArray {
+                NSWorkspace.shared.open(URL(string: items)!)
+            }
+        }
+        catch {/* error handling here */}
 
+        
+        
         // Do any additional setup after loading the view.
     }
 
